15362528df8ad33109e409e947dd96cbe551ca8b
Van den Berg <eliasvdb8@gmail.com>
2016-03-18 10:06:54 +0100

    Basic boot logica (rendering van boot en ballen en evalueren van antwoord).

--- pirate/plugin/js/pirate.js
+++ pirate/plugin/js/pirate.js
@@ -1,4 +1,34 @@
-setTimeout(function() {
-    console.log("hello dude!");
-}, 1000);
+var boot = {
+    som: "1+1",
+    tijd: 5,
+    oplossingen: [1, 2, 3, 4],
+    oplossing: 2
+};
 
+var score = 0;
+
+$(function () {
+    var voorraad = $('.voorraad')[0];
+
+    boot.oplossingen.forEach(
+        function (opl) {
+            $(voorraad).append("<div class='bal'>" + opl + "</div>");
+        }
+    );
+
+    $($(".opgave")[0]).text(boot.som);
+
+    $('.bal').on('click', function(){
+        var keuze = $(this).text();
+        if(parseInt(keuze) === boot.oplossing){
+            verhoogScore();
+        }else{
+            console.log('niet correct');
+        }
+    });
+});
+
+function verhoogScore() {
+    score++;
+    $('#scorebord').text(score + ' punten');
+}
